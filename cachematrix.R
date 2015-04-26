b77ce31e264449e8769e8226411041dbf66826ec
Jonathan <jwise@genesismedia.com>
2015-04-26 14:03:24 -0400

    Committing Programming Assignment #2 Cached Matrix Inverse code to R Programming Git Hub

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will check to see if an inverse of a matrix 
+## exists in the cache and, if so, returns the inverse of the matrix. 
+## Otherwise, the function will calculate the inverse and save it to the cache
 
-## Write a short comment describing this function
+## Here, the function creates a vector of functions to set and get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
